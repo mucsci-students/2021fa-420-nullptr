9396cd48761682826bf25ea1d3b8e3b50b290aa2
Briar <38114039+DHDodo@users.noreply.github.com>
2021-09-14 19:18:14 -0400

    CLI Skeleton (#32)
    * began work on skeleton of CLI
    
    * Quick format update as well as beginning of implementation of loop.
    
    * Did skeleton for class section of CLI.
    
    * Reformatted loop structure and implemented skeleton for attribute operations.
    
    * Finished rest of basic skeleton.
    
    * Fixed issue with skeleton loop not breaking properly.
    
    * Fixed subloop routine on 4, 5, 6. Program properly exits
    
    * Finished skeleton operation. Fixed error with strings breaking operation of interface.
    
    * Adjusted comments for basic error checking.
    
    Co-authored-by: Tyler P York <tpyork@csciarch02.muad.local>

---
+++ CLI.hpp
@@ -0,0 +1,270 @@
+/*
+  Filename   : CLI.hpp
+  Description: File which generates the command line interface
+  for the user to create UML diagrams
+*/
+
+/************************************************************/
+// Macro guard
+#ifndef CLI_HPP
+#define CLI_HPP
+/************************************************************/
+
+//--------------------------------------------------------------------
+// System includes
+#include <string>
+#include "UMLClass.hpp"
+#include "UMLAttribute.hpp"
+#include "UMLRelationship.hpp"
+#include "UMLData.hpp"
+#include "UMLFile.hpp"
+#include <vector>
+#include <iostream>
+//--------------------------------------------------------------------
+
+//--------------------------------------------------------------------
+// Using declarations
+ using std::cin;
+ using std::cout;
+ using std::endl;
+ using std::string;
+ using std::vector;
+ using std::invalid_argument;
+//--------------------------------------------------------------------
+
+class CLI
+{
+    private:
+        // stores choice input by user
+        string userChoice;
+        // loop boolean that maintains CLI routine
+        bool mainLoop;
+        // loop boolean that maintains CLI subrountines
+        bool subLoop;
+    public:
+        // displays command line interface
+        void displayCLI();
+};
+
+void CLI::displayCLI ()
+{
+    // loop boolean that maintains CLI routine
+    mainLoop = true;
+    // loop boolean that maintains CLI subrountines
+    subLoop = false;
+
+    while (mainLoop) {
+        // Main prompt: prompts user for options.
+        cout << "Choose an option:"<< endl;
+        cout << "Class [0]" << endl;
+        cout << "Attribute [1]" << endl;
+        cout << "Relationship [2]" << endl;
+        cout << "List [3]" << endl;
+        cout << "Save [4]" << endl;
+        cout << "Load [5]" << endl;
+        cout << "Help [6]" << endl;
+        cout << "Exit [7]" << endl;
+
+        // store user option
+        cout << endl << "Choice: ";
+        cin >> userChoice;
+        cout << endl;
+
+        // start subloop
+        subLoop = true;
+
+        while (subLoop) { 
+            // Class
+            if(userChoice == "0") {
+                // Lists operations for modifying classes
+                cout << "Choose an option:"<< endl;
+                cout << "Add [0]" << endl;
+                cout << "Remove [1]" << endl;
+                cout << "Rename [2]" << endl;
+                cout << "Back [3]" << endl;
+
+                // store user option
+                cout << endl << "Choice: ";
+                cin >> userChoice;
+                cout << endl;
+
+                // Add class
+                if (userChoice == "0") {
+                    // prompt name of class then add class
+                    subLoop = false;
+                }
+                // Remove class
+                else if (userChoice == "1") {
+                    // prompt name of class then remove class
+                    subLoop = false;
+                } 
+                // Rename class
+                else if (userChoice == "2") {
+                    // prompt name of class then rename class
+                    subLoop = false;
+                }
+                // Go back
+                else if (userChoice == "3") {
+                    // exits loop, goes back
+                    subLoop = false;
+                }
+                // Invalid choice
+                else {
+                    // show error, reset user choice
+                    cout << "Invalid choice!" << endl << endl;
+                    userChoice = "0";
+                }
+            }
+
+            // Attribute
+            else if(userChoice == "1") {
+                // Lists operations for modifying attributes
+                cout << "Choose an option:"<< endl;
+                cout << "Add [0]" << endl;
+                cout << "Remove [1]" << endl;
+                cout << "Rename [2]" << endl;
+                cout << "Back [3]" << endl;
+
+                // store user option
+                cout << endl << "Choice: ";
+                cin >> userChoice;
+                cout << endl;
+
+                // Add attribute
+                if (userChoice == "0") {
+                    // prompt name of class and attribute then add attribute
+                    subLoop = false;
+                }
+                // Remove class
+                else if (userChoice == "1") {
+                    // prompt name of class and attribute then remove attribute
+                    subLoop = false;
+                } 
+                // Rename class
+                else if (userChoice == "2") {
+                    // prompt name of class and attribute then rename attribute
+                    subLoop = false;
+                }
+                // Go back
+                else if (userChoice == "3") {
+                    // exits loop, goes back
+                    subLoop = false;
+                }
+                // Invalid choice
+                else {
+                    // show error, reset user choice
+                    cout << "Invalid choice!" << endl << endl;
+                    userChoice = "1";
+                }
+            }
+
+            // Relationship
+            else if(userChoice == "2") {
+                // Lists operations for modifying relationships
+                cout << "Choose an option:"<< endl;
+                cout << "Add [0]" << endl;
+                cout << "Remove [1]" << endl;
+                cout << "Back [2]" << endl;
+
+                // store user option
+                cout << endl << "Choice: ";
+                cin >> userChoice;
+                cout << endl;
+
+                // Add relationship
+                if (userChoice == "0") {
+                    // prompt name of source and destination and adds relationship
+                    subLoop = false;
+                }
+                // Remove relationship
+                else if (userChoice == "1") {
+                    // prompt name of source and destination and removes relationship
+                    subLoop = false;
+                } 
+                // Go back
+                else if (userChoice == "2") {
+                    // exits loop, goes back
+                    subLoop = false;
+                }
+                // Invalid choice
+                else {
+                    // show error, reset user choice
+                    cout << "Invalid choice!" << endl << endl;
+                    userChoice = "2";
+                }
+            }
+
+            // Diagram
+            else if(userChoice == "3") {
+                // Lists operations for viewing information within the diagram
+                cout << "Choose an option:"<< endl;
+                cout << "Class [0]" << endl;
+                cout << "Diagram [1]" << endl;
+                cout << "Back [2]" << endl;
+
+                // store user option
+                cout << endl << "Choice: ";
+                cin >> userChoice;
+                cout << endl;
+
+                // Display single class
+                if (userChoice == "0") {
+                    // prompt class name, shows information about class
+                    subLoop = false;
+                }
+                // Display all information
+                else if (userChoice == "1") {
+                    // show all information and return back to base loop
+                    subLoop = false;
+                } 
+                // Go back
+                else if (userChoice == "2") {
+                    // exits loop, goes back
+                    subLoop = false;
+                }
+                // Invalid choice
+                else {
+                    // show error, reset user choice
+                    cout << "Invalid choice!" << endl << endl;
+                    userChoice = "3";
+                }
+            }
+
+            // Save UML
+            else if (userChoice == "4") {
+                // Saves UML diagram to a JSON file in the same directory as the executable
+                subLoop = false;
+            }
+
+            // Load UML
+            else if (userChoice == "5") {
+                // Prompts for a directory. If the directory exists, UML diagram info is displayed
+                subLoop = false;
+            }
+
+            // Help
+            else if (userChoice == "6") {
+                // Loads this help file into the console
+                subLoop = false;
+            }
+
+            // Exits the program
+            else if (userChoice == "7") {
+                subLoop = false;
+                mainLoop = false;
+            }
+
+            // Invalid choice
+            else {
+                // show error, reset user choice and break loop
+                cout << "Invalid choice!" << endl << endl;
+                userChoice = "";
+                subLoop = false;
+            }
+        }  
+    } 
+}
+
+/************************************************************/
+#endif
+/************************************************************/
