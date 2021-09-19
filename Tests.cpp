83b8bf393e687d1f70627e87a227ab8b209e1965
ttpyork <89753735+ttpyork@users.noreply.github.com>
2021-09-19 19:33:00 -0400

    added tests for your program (#48)
    Co-authored-by: Tyler P York <tpyork@csciarch02.muad.local>

--- Tests.cpp
+++ Tests.cpp
@@ -1,9 +1,13 @@
 #include <gtest/gtest.h>
 #include "UMLData.hpp"
+#include "CLI.hpp"
+#include <string>
+#include <iostream>
 // TEST(TestSuiteName, TestName) {
 //   ... test body ...
 // }
 
+//Tests for UMLData.hpp
 TEST(UMLDataAddClassTest, AddClassAndGetCopy) {
     UMLData data;
     data.addClass("test");
@@ -15,3 +19,104 @@ TEST(UMLDataAddClassTest, AddClassThatAlreadyExists) {
     data.addClass("test");
     ASSERT_ANY_THROW(data.addClass("test"));
 }
+
+TEST(UMLDataRemoveClassTest, RemoveClassThatDoesntExist) {
+    UMLData data;
+    ASSERT_ANY_THROW(data.deleteClass("test"));
+}
+
+TEST(UMLDataRenameClassTest, RenameAClassWorks) {
+    UMLData data;
+    string oldName;
+    string updatedName;
+    oldName = "test";
+    updatedName = "testtest";
+    data.addClass(oldName);
+    data.changeClassName(oldName, updatedName);
+    ASSERT_EQ(updatedName, updatedName);
+}
+
+TEST(UMLDataRenameClassTest, RenameClassIntoClassThatAlreadyExists) {
+    UMLData data;
+    data.addClass("test");
+    ASSERT_ANY_THROW(data.changeClassName("test", "test"));
+}
+
+TEST(UMLDataAttributeTest, AddAttributeWorks) {
+    UMLData data;
+    data.addClass("test");
+    UMLAttribute attribute("hastest");
+    data.addClassAttribute("test", attribute);
+       for (UMLAttribute attr : data.getClassAttributes("test")) {
+           if(attr.getAttributeName() == "hastest"){
+               ASSERT_EQ(attr.getAttributeName(), "hastest");
+           }
+       }
+}
+
+
+TEST(UMLDataAttributeTest, ChangeAttributeNameWorks) {
+   
+    bool hasTestGone;
+    hasTestGone == true;
+    bool newHasTestPresent;
+    newHasTestPresent == false;
+
+    UMLData data;
+    data.addClass("test");
+    UMLAttribute attribute("hastest");
+    data.addClassAttribute("test", attribute);
+    data.changeAttributeName("test", "hastest", "newHasTest");
+
+        for (UMLAttribute attr : data.getClassAttributes("test")) {
+           if(attr.getAttributeName() == "hastest"){
+               hasTestGone == false;
+           }
+            if(attr.getAttributeName() == "newHasTest"){
+              newHasTestPresent == true;
+           }
+       }
+      ASSERT_EQ(hasTestGone, newHasTestPresent);  
+}
+
+
+TEST(UMLDataAttributeTest, RemovingNonExistantAttribute) {
+    UMLData data;
+    data.addClass("test");
+    UMLAttribute attribute("hastestt");
+    ASSERT_ANY_THROW(data.removeClassAttribute("test", "hastestt"));
+}
+
+/*
+TEST(UMLDataAttributeTest, RemovingAttributeWorks) {
+    UMLData data;
+    data.addClass("test");
+    UMLAttribute attribute("hastest");
+    data.addClassAttribute("test", attribute);
+    data.removeClassAttribute("test", attribute);
+    ASSERT_EQ(data.getClassCopy("test").removeClassAttribute("test", attribute), "");
+
+}
+*/
+
+//Attribute tests
+/*
+-Removing an attribute works
+-removing an attribute  that doestn exist should throw an error
+- renaming an attribute should work
+- renaming an attribute to another attribute that already exists should throw an error
+*/
+
+
+ //adds class attribute to specified className
+     //   void addClassAttribute(string className, UMLAttribute attribute);
+
+/*
+TEST(SquareRootTest, PositiveNos) { 
+    EXPECT_EQ (18.0, square‑root (324.0));
+    EXPECT_EQ (25.4, square‑root (645.16));
+    EXPECT_EQ (50.3321, square‑root (2533.310224));
+}
+*/
+
+
