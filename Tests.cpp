4e290160da312cbff63bec433505713ece9aedaa
Tyler P York <tpyork@csciarch02.muad.local>
2021-09-19 21:36:49 -0400

    Worked more on Unit Tests

--- Tests.cpp
+++ Tests.cpp
@@ -1,8 +1,11 @@
 #include <gtest/gtest.h>
 #include "UMLData.hpp"
 #include "CLI.hpp"
+#include "UMLRelationship.hpp"
+#include "UMLClass.hpp"
 #include <string>
 #include <iostream>
+
 // TEST(TestSuiteName, TestName) {
 //   ... test body ...
 // }
@@ -54,7 +57,6 @@ TEST(UMLDataAttributeTest, AddAttributeWorks) {
        }
 }
 
-
 TEST(UMLDataAttributeTest, ChangeAttributeNameWorks) {
    
     bool hasTestGone;
@@ -87,36 +89,68 @@ TEST(UMLDataAttributeTest, RemovingNonExistantAttribute) {
     ASSERT_ANY_THROW(data.removeClassAttribute("test", "hastestt"));
 }
 
+
+/*
+TEST(UMLDataRelationshipTest, AddingRelationshipWorks) {
+   
+    bool haveRelationship;
+    haveRelationship == false;
+    UMLData data;
+    data.addClass("test");
+    data.addClass("test1");
+    vector<UMLRelationship> rel = data.getRelationships();
+    
+
+
+    data.addRelationship("test", "test1");
+   
+
+
+
+   for(auto i = rel.begin(); i != rel.end(); ++i){
+
+           if(&(rel[i].getSource()) == "test1"){
+          
+             haveRelationship == true;
+           }
+           else if(&(rel[i].getDestination()) == "test1"){
+               haveRelationship == true;
+           }
+   }
+  
+
+
+    ASSERT_EQ(haveRelationship, true); 
+
+}
+*/
+
+
 /*
 TEST(UMLDataAttributeTest, RemovingAttributeWorks) {
+      
+    bool hasTestGone;
+    hasTestGone == true;
+    
     UMLData data;
     data.addClass("test");
     UMLAttribute attribute("hastest");
     data.addClassAttribute("test", attribute);
-    data.removeClassAttribute("test", attribute);
-    ASSERT_EQ(data.getClassCopy("test").removeClassAttribute("test", attribute), "");
+    data.removeClassAttribute("test", "hastest");
+  
+  
+   for (UMLAttribute attr : data.getClassAttributes("test")) {
+           if(attr.getAttributeName() == "hastest"){
+              hasTestGone == false;
+              
+           
+           }
+       }
+    ASSERT_EQ(hasTestGone, true); 
+ */
+    
 
-}
-*/
-
-//Attribute tests
-/*
--Removing an attribute works
--removing an attribute  that doestn exist should throw an error
-- renaming an attribute should work
-- renaming an attribute to another attribute that already exists should throw an error
-*/
 
 
- //adds class attribute to specified className
-     //   void addClassAttribute(string className, UMLAttribute attribute);
-
-/*
-TEST(SquareRootTest, PositiveNos) { 
-    EXPECT_EQ (18.0, square‑root (324.0));
-    EXPECT_EQ (25.4, square‑root (645.16));
-    EXPECT_EQ (50.3321, square‑root (2533.310224));
-}
-*/
 
 
