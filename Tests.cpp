1eb5863039390c84b9dfa619841a5fc4c54496a4
ttpyork <89753735+ttpyork@users.noreply.github.com>
2021-09-19 22:01:16 -0400

    Uml tests (#52)
    * added tests for your program
    
    * Worked more on Unit Tests
    
    Co-authored-by: Tyler P York <tpyork@csciarch02.muad.local>
    Co-authored-by: NicatFire <70117793+NicatFire@users.noreply.github.com>

--- Tests.cpp
+++ Tests.cpp
@@ -57,7 +57,6 @@ TEST(UMLDataAttributeTest, AddAttributeWorks) {
        }
 }
 
-
 TEST(UMLDataAttributeTest, ChangeAttributeNameWorks) {
    
     bool hasTestGone;
@@ -91,6 +90,38 @@ TEST(UMLDataAttributeTest, RemovingNonExistantAttribute) {
 }
 
 /*
+TEST(UMLDataRelationshipTest, AddingRelationshipWorks) {
+   
+    bool haveRelationship;
+    haveRelationship == false;
+    UMLData data;
+    data.addClass("test");
+    data.addClass("test1");
+    vector<UMLRelationship> rel = data.getRelationships();
+    
+
+
+    data.addRelationship("test", "test1");
+   
+
+
+
+   for(auto i = rel.begin(); i != rel.end(); ++i){
+
+           if(&(rel[i].getSource()) == "test1"){
+          
+             haveRelationship == true;
+           }
+           else if(&(rel[i].getDestination()) == "test1"){
+               haveRelationship == true;
+           }
+   }
+  
+
+
+    ASSERT_EQ(haveRelationship, true); 
+
+/*
 TEST(UMLDataAttributeTest, RemovingAttributeWorks) {
     UMLData data;
     data.addClass("test");
@@ -121,6 +152,34 @@ TEST(SquareRootTest, PositiveNos) {
     EXPECT_EQ (50.3321, square‑root (2533.310224));
 }
 */
+
+
+/*
+TEST(UMLDataAttributeTest, RemovingAttributeWorks) {
+      
+    bool hasTestGone;
+    hasTestGone == true;
+    
+    UMLData data;
+    data.addClass("test");
+    UMLAttribute attribute("hastest");
+    data.addClassAttribute("test", attribute);
+    data.removeClassAttribute("test", "hastest");
+  
+  
+   for (UMLAttribute attr : data.getClassAttributes("test")) {
+           if(attr.getAttributeName() == "hastest"){
+              hasTestGone == false;
+              
+           
+           }
+       }
+    ASSERT_EQ(hasTestGone, true); 
+ */
+    
+
+
+
 
 
 //Tests for UMLRelationship.hpp
