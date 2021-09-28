a02be377a5a48222bfa43bbb9e85063bc8856a98
logan.zug@gmail.com <loganified@gmail.com>
2021-09-28 22:39:33 +0000

    added http library, some testing in main, added tempaltes folder

--- main.cpp
+++ main.cpp
@@ -6,17 +6,27 @@
 
 //--------------------------------------------------------------------
 // System includes
-#include <string>
-#include <iostream>
-#include <vector>
-#include "UMLClass.hpp"
-#include "UMLFile.hpp"
-#include "UMLData.hpp"
 #include "CLI.hpp"
+#include "include/httplib/httplib.h"
 //--------------------------------------------------------------------
 
 int main()
 {
+    httplib::Server svr;
+    svr.Get("/", [](const httplib::Request&, httplib::Response& res) {
+      res.set_redirect("/hi");
+    });
+    svr.Get("/hi", [](const httplib::Request&, httplib::Response& res) {
+      res.set_content("<h1>hello there!</h1>", "text/html");
+    });
+
+    std::cout << "running at http:://localhost:8080/" << std::endl;
+
+
+    svr.listen("localhost", 8080);
+
+    
+
     CLI newInterface;
     newInterface.displayCLI();
     return 0;
