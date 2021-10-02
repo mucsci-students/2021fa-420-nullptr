c012050331c0efff0cee88e7f27cd6cf3d1676aa
DHDodo <bumpmytoast@gmail.com>
2021-10-02 20:44:01 +0000

    Added relationship functionality to UMLRelationship. Incomplete so it will not compile.

--- UMLRelationship.cpp
+++ UMLRelationship.cpp
@@ -9,9 +9,10 @@
 //--------------------------------------------------------------------
 
 // Constructor for class objects
-UMLRelationship::UMLRelationship(const UMLClass& src, const UMLClass& dest)
+UMLRelationship::UMLRelationship(const UMLClass& src, const UMLClass& dest, Type type)
 :source(&src)
 ,destination(&dest)
+,relationshipType(type)
 {
 }
 
@@ -25,4 +26,15 @@ const UMLClass& UMLRelationship::getSource() const
 const UMLClass& UMLRelationship::getDestination() const
 {
 	return *destination;
+}
+
+// Grab type of relationship
+Type UMLRelationship::getType() const
+{
+  return relationshipType;
+}
+
+// Set type of relationship
+void UMLRelationship::setType(Type newType) {
+  relationshipType = newType;
 }
--- include/UMLRelationship.hpp
+++ include/UMLRelationship.hpp
@@ -2,8 +2,7 @@
 /*
   Filename   : UMLRelationship.hpp
   Description: Serves as an object for which information about a relationship
-  in the UML diagram is stored. To be further expanded with different types
-  of relationships.
+  in the UML diagram is stored, alongside its relationship type.
 */
 
 //--------------------------------------------------------------------
@@ -12,20 +11,37 @@
 #include "UMLClass.hpp"
 //--------------------------------------------------------------------
 
+//--------------------------------------------------------------------
+/* Types of relationships 
+Aggregation: shared, ownerless collection 
+Composition: unshared, 'has-a' relationship collection
+Generalization: inheritance relationship
+Realization: interface implementation relationship */
+//--------------------------------------------------------------------
+enum Type {aggregation, composition, generalization, realization};
+
 class UMLRelationship
 {
 	private:
 		// Name of class that owns attribute and name of attribute
 		const UMLClass* source;
 		const UMLClass* destination;
+		// Type of relationship
+		Type relationshipType;
 
 	public:
 		// Constructor for class objects
-		UMLRelationship(const UMLClass& src, const UMLClass& dest);
+		UMLRelationship(const UMLClass& src, const UMLClass& dest, Type type);
 
 		// Grab name of the source class
 		const UMLClass& getSource() const;
 
-		// Grab name of the destination classs
+		// Grab name of the destination class
 		const UMLClass& getDestination() const;
+
+		// Grab type of relationship
+		Type getType() const;
+
+		// Set type of relationship
+		void setType(Type newType);
 };
