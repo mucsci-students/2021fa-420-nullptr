00e97437d07055bb78a06cd6b5bef0a718e08208
NicatFire <nicatfire8880@gmail.com>
2021-10-05 00:09:48 -0400

    Added UMLMethod declaration and implementation, and updated cmake list

---
+++ UMLMethod.cpp
@@ -0,0 +1,60 @@
+/*
+  Filename   : UMLMethod.cpp
+  Description: Implementation of a method.
+*/
+
+//--------------------------------------------------------------------
+// System includes
+#include <string>
+#include <vector>
+#include "include/UMLMethod.hpp"
+//--------------------------------------------------------------------
+
+//--------------------------------------------------------------------
+// Using declarations
+using std::string;
+using std::vector;
+//--------------------------------------------------------------------
+
+UMLMethod::UMLMethod(string newName, string newReturnType, vector<string> newParam)
+:name(newName)
+,returnType(newReturnType)
+,param(newParam)
+{
+}
+
+string 
+UMLMethod::getName()
+{
+	return name;
+}
+
+string
+UMLMethod::getReturnType()
+{
+	return returnType;
+}
+
+vector<string>
+UMLMethod::getParam()
+{
+	return param;
+}
+
+void
+UMLMethod::setName(string newName)
+{
+	name = newName;
+}
+
+void
+UMLMethod::setReturnType(string newReturnType)
+{
+	returnType = newReturnType;
+}
+
+void
+UMLMethod::setParam(vector<string> newParam)
+{
+	param = newParam;
+}
---
+++ include/UMLMethod.hpp
@@ -0,0 +1,61 @@
+#pragma once
+/*
+  Filename   : UMLMethod.hpp
+  Description: Serves as an object for which information about a method 
+	is stored.
+*/
+
+// --------------------------------------------------------------------
+//  System includes
+#include <string>
+#include <vector>
+// --------------------------------------------------------------------
+
+// --------------------------------------------------------------------
+//  Using declarations
+using std::string;
+using std::vector;
+// --------------------------------------------------------------------
+
+class UMLMethod
+{
+	private:
+
+		// Name of method
+		string name;
+
+		// Type of the returned value
+		string returnType;
+
+		// List of parameters for the method
+		vector<string> param;
+
+	public:
+
+		// Creates a UMLMethod object with the constructor's parameters as its fields
+		UMLMethod(string newName, string newReturnType, vector<string> newParam);
+
+		// Returns the method's name
+		string 
+		getName();
+
+		// Returns the method's return type
+		string
+		getReturnType();
+
+		// Returns a list of all of the method's parameters
+		vector<string>
+		getParam();
+
+		// Changes the method's name to match the parameter
+		void
+		setName(string newName);
+
+		// Changes the method's return type to match the parameter
+		void
+		setReturnType(string newReturnType);
+
+		// Changes the list of the method's parameters to match the parameter
+		void
+		setParam(vector<string> newParam);
+};
