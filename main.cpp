1c9e5a46f0e88b82e58369ba989ead1a11075b68
lgnzg <logan.zug@gmail.com>
2021-10-19 00:12:12 +0000

    test

--- main.cpp
+++ main.cpp
@@ -19,25 +19,6 @@
 int main(int argc, char** argv)
 {
 
-    //  inja::Environment env;
-    //  inja::Template temp = env.parse_template("../templates/test.html");
-    //  UMLData data;
-    //  data = UMLFile("save.json").load();
-    //  json j = data.getJson();
-    //  j["errors"] = json::array();
-    //  j["success"] = json::array();
-    //  j["files"] = json::array();
-     
-    
-
-    //       std::cout << j << std::endl;
-    //       int count = 0;
-    //       for (auto attr : data.getClassAttributes("fish"))
-    //       {
-    //         std::cout << count << ". " <<  attr->getAttributeName() << std::endl;
-    //         ++count;
-    //       }
-
 
 
     // Parameter check to see if user wants CLI or GUI
