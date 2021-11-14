e4176f33d036ea235475d9d59492199b531566db
DHDodo <bumpmytoast@gmail.com>
2021-11-14 19:36:32 +0000

    Implemented root menu skeleton. Still WIP.

--- include/CLI.hpp
+++ include/CLI.hpp
@@ -20,8 +20,6 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
-#include <memory>
-#include <list>
 //--------------------------------------------------------------------
 
 //--------------------------------------------------------------------
@@ -37,15 +35,133 @@
  using std::map;
 //--------------------------------------------------------------------
 
+/********************/
+//Typedefs
+
+typedef shared_ptr<UMLAttribute> attr_ptr;
+typedef shared_ptr<UMLMethod> method_ptr;
+typedef long unsigned int size_t;
+
 class CLI
 {
   private:
+    
+    /********************/
+    //Global variables
 
-  // No private methods 
+    // Error check to prevent 'success' print 
+    bool ErrorStatus;
+
+    // Main UML data object storing UML stuff
+    UMLData Model;
+
+    // Allows for undo/redo
+    UMLDataHistory History {Model};
+
+    /********************/
+    //Adding
+
+    bool add_field(string className);
+    bool add_method(string className);
+    bool add_parameter(string className, method_ptr methodIter);
+
+
+    /********************/
+    //Deleting
+    
+    void delete_field(string className);
+    void delete_method(string className);
+    void delete_parameter(string className, method_ptr methodIter);
+
+
+    /********************/
+    //Renaming
+    
+    void rename_field(string className);
+    void rename_method(string className);
+    void rename_parameter(method_ptr methodIter);
+
+    /********************/
+    //Type changing
+
+    void change_field(string className);
+    void change_method(string className);
+    void change_parameter(string className, method_ptr methodIter);
+
+    /********************/
+    //Display functions
+
+    void display_class(UMLClass currentClass);
+    void display_method(attr_ptr methodIter); 
+    void display_relationship(string source, string destination, string rType);
+    
+    /********************/
+    //Input parsing
+
+    size_t user_int_input();
+    string tab_completion();
+
+
+    /********************/
+    //Undo/Redo
+
+    void undo();
+    void redo();
+
+    /********************/
+    //Misc.
+
+    method_ptr select_method(string className, string methodName);
+    attr_ptr select_field(string className, string fieldName);
 
   public:
+    
+    // Takes in user input and calls different functions based on
+    // what command the user typed.
+    void cli_menu();
 
-    // Test for seeing if example menu works
-    int cli_menu();
+    // Lists all classes the user has created.
+    void list_classes();
 
+    // Lists all relationships the user has created.
+    void list_relationships();
+
+    // User creates and names a class and may give it any number
+    // of attributes.
+    void create_class();
+
+    // Creates a new relationship between classes.
+    void create_relationship();
+
+    // User will be prompted to type in the class name,
+    // and if it exists, it will be deleted.
+    void delete_class();
+
+    // User will be prompted to type in the source and destination,
+    // and if the relationship exists, it will be deleted.
+    void delete_relationship();
+
+    // User types in the current class name, and then the name
+    // they'd like to change it to. Then it gets renamed.
+    void rename_class();
+
+    // User will be prompted to type in the source, destination,
+    // and NEW type. Then the relationship type will be changed.
+    void change_relationship();
+
+    // Does various things with fields based on user input.
+    void edit_fields();
+
+    // Does various things with methods based on user input.
+    void edit_methods();
+
+    // Does various things with parameters based on user input.
+    void edit_parameters(string className);
+
+    // Saves the user's progress into a json file.
+    void save_uml();
+
+    // Loads a json save file, overwriting the current session.
+    void load_uml();
+   
 };
